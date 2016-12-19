cfb492e6493ae730b209e69a333b254dc925d87f
John Goode <john@johngoode.com>
2016-12-19 13:06:22 +0000

    Create relaydefinitions.py

---
+++ relaydefinitions.py
@@ -0,0 +1,15 @@
+# bury this in a sub folder
+relays = [
+    { 'id' : 1, 'name' : 'hot', 'state' : 'off'},
+    { 'id' : 2, 'name' : 'warm', 'state' : 'off'},
+    { 'id' : 3, 'name' : 'cold', 'state' : 'on'},
+    #{ 'id' : 4, 'name' : 'temp', 'state' : 'value'}
+    ]
+
+
+relayIdToPin = {
+    1 : 12,
+    2 : 16,
+    3 : 20,
+    #4 : 15
+    }
