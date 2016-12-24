7b7e63d10fd838b51356ba4ae94d122bb71e5a0a
John Goode <john@johngoode.com>
2016-12-24 13:58:44 +0000

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -1,5 +1,3 @@
-# use sudo crontab -e to run this programme every minute
-# Add this file to the top level of your Sender Pi
 # Distributed with a free-will license.
 # Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
 # MPL3115A2
@@ -9,96 +7,98 @@
 import smbus
 import time
 import requests
+from ubidots import ApiClient
 
 #while True:
-
+api = ApiClient("token here")
 # Get I2C bus
-        bus = smbus.SMBus(1)
+bus = smbus.SMBus(1)
 
 # MPL3115A2 address, 0x60(96)
 # Select control register, 0x26(38)
 #               0xB9(185)       Active mode, OSR = 128, Altimeter mode
-        bus.write_byte_data(0x60, 0x26, 0xB9)
+
+bus.write_byte_data(0x60, 0x26, 0xB9)
 # MPL3115A2 address, 0x60(96)
 # Select data configuration register, 0x13(19)
 #               0x07(07)        Data ready event enabled for altitude, pressure, temperature
-        bus.write_byte_data(0x60, 0x13, 0x07)
+bus.write_byte_data(0x60, 0x13, 0x07)
 # MPL3115A2 address, 0x60(96)
 # Select control register, 0x26(38)
 #               0xB9(185)       Active mode, OSR = 128, Altimeter mode
-        bus.write_byte_data(0x60, 0x26, 0xB9)
+bus.write_byte_data(0x60, 0x26, 0xB9)
 
-        time.sleep(1)
+time.sleep(1)
 
 # MPL3115A2 address, 0x60(96)
 # Read data back from 0x00(00), 6 bytes
 # status, tHeight MSB1, tHeight MSB, tHeight LSB, temp MSB, temp LSB
-        data = bus.read_i2c_block_data(0x60, 0x00, 6)
+data = bus.read_i2c_block_data(0x60, 0x00, 6)
 
 # Convert the data to 20-bits
-        tHeight = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
-        temp = ((data[4] * 256) + (data[5] & 0xF0)) / 16
-        altitude = tHeight / 16000.0
-        cTemp = temp / 16.0
-        fTemp = cTemp * 1.8 + 32
+#tHeight = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
+temp = ((data[4] * 256) + (data[5] & 0xF0)) / 16
+#altitude = tHeight / 16000.0
+cTemp = temp / 16.0
+#fTemp = cTemp * 1.8 + 32
 
 # MPL3115A2 address, 0x60(96)
 # Select control register, 0x26(38)
 #               0x39(57)        Active mode, OSR = 128, Barometer mode
-        bus.write_byte_data(0x60, 0x26, 0x39)
+bus.write_byte_data(0x60, 0x26, 0x39)
 
-        time.sleep(1)
+time.sleep(1)
 
 # MPL3115A2 address, 0x60(96)
 # Read data back from 0x00(00), 4 bytes
 # status, pres MSB1, pres MSB, pres LSB
-        data = bus.read_i2c_block_data(0x60, 0x00, 4)
+data = bus.read_i2c_block_data(0x60, 0x00, 4)
 
 # Convert the data to 20-bits
-        pres = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
-        pressure = (pres / 4.0) / 100.0
+#pres = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
+#pressure = (pres / 4.0) / 100.0
 
 # Output data to screen
-        print "Pressure : %.2f MBar" %pressure
+#print "Pressure : %.2f MBar" %pressure
         #print "Altitude : %.2f m" %altitude
-        print "Temperature in Celsius  : %.2f C" %cTemp
+print "Temperature in Celsius  : %.2f C" %cTemp
         #print "Temperature in Fahrenheit  : %.2f F" %fTemp
 
-        if cTemp > 19:
+api.save_collection([
+                {'variable': 'kjhfskjhsfd', 'value': cTemp},
+                ])
+
+#api.save_collection([
+#                {'variable': 'kjsdkjsdhkjhasdkjh', 'value': pressure},
+#                ])
+
+
+if cTemp > 19:
         r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/1', verify=False, json={"state": "on"})
         headers = {'Content-type': 'application/json'}
-                #GPIO.output(7, True)
-                #time.sleep(5)
-                #GPIO.output(7, False)
-                #print(r.status_code)
+
         r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/2', verify=False, json={"state": "off"})
         headers = {'Content-type': 'application/json'}
-                #print(r.status_code)
+
         r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/3', verify=False, json={"state": "off"})
         headers = {'Content-type': 'application/json'}
-                #print(r.status_code)
-if ((cTemp >= 16.8) and (cTemp <= 19)):
+
+if ((cTemp >= 17.8) and (cTemp <= 19)):
         r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/2', verify=False, json={"state": "on"})
         headers = {'Content-type': 'application/json'}
-                #GPIO.output(8, True)
-                #time.sleep(5)
-                #GPIO.output(8, False)                
-                #print(r.status_code)
+
         r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/1', verify=False, json={"state": "off"})
         headers = {'Content-type': 'application/json'}
-                #print(r.status_code)
+
         r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/3', verify=False, json={"state": "off"})
         headers = {'Content-type': 'application/json'}
-                #print(r.status_code)
-if cTemp < 16.8:
+
+if cTemp < 17.8:
         r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/3', verify=False, json={"state": "on"})
         headers = {'Content-type': 'application/json'}
-                #GPIO.output(25, True)
-                #time.sleep(5)
-                #GPIO.output(25, False)                
-                #print(r.status_code)
+
         r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/1', verify=False, json={"state": "off"})
         headers = {'Content-type': 'application/json'}
-                #print(r.status_code)
+
         r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/2', verify=False, json={"state": "off"})
         headers = {'Content-type': 'application/json'}
