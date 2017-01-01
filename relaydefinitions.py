5433bed9e8db5cb95067e5bce4d2594dbb26968c
John Goode <john@johngoode.com>
2017-01-01 20:23:41 +0000

    Update relaydefinitions.py

--- relaydefinitions.py
+++ relaydefinitions.py
@@ -3,13 +3,15 @@ relays = [
     { 'id' : 1, 'name' : 'hot', 'state' : 'off'},
     { 'id' : 2, 'name' : 'warm', 'state' : 'off'},
     { 'id' : 3, 'name' : 'cold', 'state' : 'on'},
-    #{ 'id' : 4, 'name' : 'temp', 'state' : 'value'}
+    { 'id' : 4, 'name' : '60mins', 'state' : 'off'},
+    { 'id' : 5, 'name' : 'temp', 'state' : 'value'},
     ]
-
 
 relayIdToPin = {
     1 : 12,
     2 : 16,
     3 : 20,
-    #4 : 15
+    4 : 23,
+    5 : 4,
     }
+
