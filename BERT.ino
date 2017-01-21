bd76a81c36991b24be8d6f9f1009dec6a57c1c2b
John Goode <john@johngoode.com>
2017-01-21 18:38:43 +0000

    Update BERT.ino

--- BERT.ino
+++ BERT.ino
@@ -1,87 +1,92 @@
-// Variables
+// This #include statement was automatically added by the Particle IDE.
+#include "Ubidots/Ubidots.h"
 
-//Servo myservo; 
-                                            //was D1. Arduino timer finished, hot = 0, constant heating is off
-int led0 = D4;                              //got a call from IFTTT to turn the constant heating off
-int led = D5;                               //60mins of constant heat request led
+#define TOKEN "B5msZILvVRpLzEM2DjVYcGORlfEPpd"  // Put here your Ubidots TOKEN
+                                            
+int pushButtonState; 
+int led = D5;                               //constant heat request led
 int hot = 0;
-int pushButton = D6;                        //from Arduino and, later-on, from a physical button
-//int pos = 0; 
-//int analogvalue1;
-//int sensor = A0;
+int pushButton = D6;                        //a physical cancel button
+int analogvalue;                            //kitchen
+int analogvalue1;                           //tank
+int analogvalue2;                           //boiler
+int analogvalue3;                           //external
+int sensor = A0;                            //kitchen
+int sensor1 = A1;                           //tank
+int sensor2 = A2;                           //boiler
+int sensor3 = A3;                           //external
+unsigned int countHeatOnTime;
+unsigned int measureTempTime;
+
+
+Ubidots ubidots(TOKEN);
 
 void setup()
 {
     //Serial.begin(9600);
+    pinMode(sensor,INPUT);
+    pinMode(sensor1,INPUT);
+    pinMode(sensor2,INPUT);
+    pinMode(sensor3,INPUT);
     Particle.function("gong", gong);
     pinMode(led,OUTPUT);  
-    pinMode(led0,OUTPUT);
-    //pinMode(led1,OUTPUT);
     Particle.variable("hot", hot);
     pinMode(pushButton, INPUT_PULLDOWN);  
     hot = 0;
-    //myservo.attach(D0);
-    //pinMode(sensor,INPUT);
-    //Particle.variable("analogvalue1", analogvalue1);
+    Particle.variable("analogvalue", analogvalue);
+    Particle.variable("analogvalue1", analogvalue1);
+    Particle.variable("analogvalue2", analogvalue2);
+    Particle.variable("analogvalue3", analogvalue3);
+    measureTempTime = 0;
+    countHeatOnTime = 0;
 }
 
 
 
 int gong(String command)
 {
-    if(command == "60mins")                 //IFTTT sends 60mins to gong, ie here, which sets hot = 1; This command is issued by req.py on c-Pi
+    if(command == "60mins")                 //IFTTT sends 60mins to gong, ie here, which sets hot = 1; This command is received by req.py on c-Pi
     {                               
-        digitalWrite(led, HIGH);            //D5 was D1
-        delay(2000);
-        digitalWrite(led, LOW);             //D5
-        //digitalWrite(led1, LOW);          //
+        digitalWrite(led, HIGH);            //D5
         hot = 1;                            //  /todo-api/req.py listens for hot = 1; then sets WebRelay/api/relays/4 state = on (GPIO 23)
-        delay(5000);                        //  /todo-api/req.py runs every minute via cron job on c-Pi, this delay will overlap that 1 min delay
-        //digitalWrite(led, LOW);           //D5 
+        countHeatOnTime = millis() + 1800000; // set the time to end constant heat
     }
     
     if(command == "cancelCall")             //IFTTT variable to cancel the 60 mins of heat and, later-on, a push button too
     {                            
-        //digitalWrite(led0, HIGH);         //D4
-        hot = 0; 
-        delay(5000);
-        //digitalWrite(led0, LOW);
+        hot = 0;
+        digitalWrite(led, LOW);
     }
 }
 
 void loop()
 {
-//  analogvalue1 = analogRead(sensor);
-  
-//  if (analogvalue1 > 240)
-//  {
-//      myservo.write((analogvalue1 - 200)/1.2);    // (3.3 * analogRead(tempPin) * 100.0) / 1024;
-//      delay(150);
-//  }
-//  else
-//  if (analogvalue1 < 240)
-//  {
-//      myservo.write(20);
-//      delay(20); 
-//  }
-//  else
-//  if (analogvalue1 > 380)
-//  {
-//      myservo.write(20);
-//      delay(20); 
-//  }
-  
- 
-  int pushButtonState; 
+  if (millis() > countHeatOnTime) {
+	hot = 0; 
+	digitalWrite(led, LOW); 
+    }
+    
+  if (millis() > measureTempTime) {
+    analogvalue = analogRead(sensor);
+    ubidots.add("kitchen", analogvalue);      
 
+    analogvalue1 = analogRead(sensor1);
+    ubidots.add("tank", analogvalue1);
+
+    analogvalue2 = analogRead(sensor2);
+    ubidots.add("boiler", analogvalue2);
+
+    analogvalue3 = analogRead(sensor3);
+    ubidots.add("external", analogvalue3);
+    ubidots.sendAll(); 
+    measureTempTime = millis() + 60000;     // read sensors again in 1 minute  
+    }
+
+  
   pushButtonState = digitalRead(pushButton);//D6
 
-  if(pushButtonState == HIGH)
-  {                                         // If we push down on the push button or receive an input from Arduino pin 8...
-    hot = 0;                                // set hot to zero to cancel constant heat, this is done using LEDPIN13 on Arduino. 
-                                            // Arduino reset causes ledpin13 to go high which sets hot = 0
-    //digitalWrite(led1, HIGH);             //
-    delay(20);
-    //digitalWrite(led1, LOW);
+  if(pushButtonState == HIGH) {             // If we push down on the push button
+    hot = 0;                                //  
+    digitalWrite(led, LOW); 
   }
 }
