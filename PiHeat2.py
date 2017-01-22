a467a4a90534be0f5eaaddd3ac7d37825b1a2534
John Goode <john@johngoode.com>
2017-01-22 19:08:18 +0000

    Update PiHeat2.py

--- PiHeat2.py
+++ PiHeat2.py
@@ -236,45 +236,55 @@ while True:
         time.sleep(3)
         mylcd.lcd_clear()
 
+#Check to see if it's time to run the appliance
+
+        if (datetime.datetime.now().strftime(dateString) >= OnTimeAM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OffTimeAM[currDay].strftime(dateString)):
+          whenNext = OnTimeAM
+          whenNextOff = OffTimeAM
         if (datetime.datetime.now().strftime(dateString) < OnTimeAM[currDay].strftime(dateString)):
           mylcd.lcd_display_string(OnTimeAM[currDay].strftime(dateString2), 2)
           mylcd.lcd_display_string("next on time (1)", 1)
-          whenNext = OnTimeAM
-          whenNextOff = OffTimeAM
+
+        if (datetime.datetime.now().strftime(dateString) >= OnTime2AM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OffTime2AM[currDay].strftime(dateString)):
+          whenNext = OnTime2AM
+          whenNextOff = OffTime2AM
         if (datetime.datetime.now().strftime(dateString) > OffTimeAM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OnTime2AM[currDay].strftime(dateString)):
           mylcd.lcd_display_string(OnTime2AM[currDay].strftime(dateString2), 2)
           mylcd.lcd_display_string("next on time (2)", 1)
-          whenNext = OnTime2AM
-          whenNextOff = OffTime2AM
+
+        if (datetime.datetime.now().strftime(dateString) >= OnTime3AM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OffTime3AM[currDay].strftime(dateString)):
+          whenNext = OnTime3AM
+          whenNextOff = OffTime3AM
         if (datetime.datetime.now().strftime(dateString) > OffTime2AM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OnTime3AM[currDay].strftime(dateString)):
           mylcd.lcd_display_string((OnTime3AM[currDay].strftime(dateString)), 2)
           mylcd.lcd_display_string("next on time (3)", 1)
-          whenNext = OnTime3AM
-          whenNextOff = OffTime3AM
+
+        if (datetime.datetime.now().strftime(dateString) >= OnTimePM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OffTimePM[currDay].strftime(dateString)):
+          whenNext = OnTimePM
+          whenNextOff = OffTimePM
         if (datetime.datetime.now().strftime(dateString) > OffTime3AM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OnTimePM[currDay].strftime(dateString)):
           mylcd.lcd_display_string((OnTimePM[currDay].strftime(dateString2)), 2)
           mylcd.lcd_display_string("next on time (4)", 1)
-          whenNext = OnTimePM
-          whenNextOff = OffTimePM
+
+        if (datetime.datetime.now().strftime(dateString) >= OnTime2PM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OffTime2PM[currDay].strftime(dateString)):
+          whenNext = OnTime2PM
+          whenNextOff = OffTime2PM
         if (datetime.datetime.now().strftime(dateString) > OffTimePM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OnTime2PM[currDay].strftime(dateString)):
           mylcd.lcd_display_string((OnTime2PM[currDay].strftime(dateString2)), 2)
           mylcd.lcd_display_string("next on time (5)", 1)
-          whenNext = OnTime2PM
-          whenNextOff = OffTime2PM
+
+        if (datetime.datetime.now().strftime(dateString) >= OnTime3PM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OffTime3PM[currDay].strftime(dateString)):
+          whenNext = OnTime3PM
+          whenNextOff = OffTime3PM
         if (datetime.datetime.now().strftime(dateString) > OffTime2PM[currDay].strftime(dateString) and datetime.datetime.now().strftime(dateString) < OnTime3PM[currDay].strftime(dateString)):
           mylcd.lcd_display_string((OnTime3PM[currDay].strftime(dateString2)), 2)
           mylcd.lcd_display_string("next on time (6)", 1)
-          whenNext = OnTime3PM
-          whenNextOff = OffTime3PM
+
         if (datetime.datetime.now().strftime(dateString) > OffTime3PM[currDay].strftime(dateString)):
           mylcd.lcd_display_string((OnTimeAM[tomDay].strftime(dateString2)), 2)
           mylcd.lcd_display_string("next on tomorrow", 1)
         time.sleep(5)
         mylcd.lcd_clear()
-
-
-#Check to see if it's time to run the appliance for the AM hours
-
 
 #switch on       
         while (whenNext[currDay].strftime(dateString) <= datetime.datetime.now().strftime(dateString) and datetime.datetime.now().strftime(dateString) <= whenNextOff[currDay].strftime(dateString)):
