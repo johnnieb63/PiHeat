f096ec8cc306cd9ce98ec09872b6b6498d3f8afa
John Goode <john@johngoode.com>
2017-01-26 00:47:03 +0000

    Update req.py

--- req.py
+++ req.py
@@ -1,8 +1,5 @@
-# run this on c-Pi in /todo-api. Use a cronjob to execute every minute. req.py chats to the Photon.
-# currently IFTTT is used to provide 60mins of constant heat input signals to the Photon via DO Buttons: one for ON, the other to CANCEL
-
-from urllib2 import Request, urlopen, URLError #import libraries
-import json #import json library
+from urllib2 import Request, urlopen, URLError
+import json
 import time
 import requests
 import urllib
@@ -10,22 +7,17 @@ from flask import Flask, jsonify, abort, request, render_template
 from relaydefinitions import relays, relayIdToPin
 
 
-
-#while True:
-#   time.sleep(60)      
 try:
-            url = "https://api.particle.io/v1/devices/device_number/hot?access_token=access_token_blah_blah_blah"
+            url = "https://api.particle.io/v1/devices/your device id/hot?access_token=your access token"
 
             json_string = urllib.urlopen(url).read()
             parsed_json = json.loads(json_string)
-#           print (parsed_json['result'])
             time.sleep(2)
 
             if (parsed_json['result']) == 0:
-                r = requests.put('http://youripaddress:80/WebRelay/api/relays/4', verify=False, json={"state": "off"})
+                r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/4', verify=False, json={"state": "off"})
                 headers = {'Content-type': 'application/json'}
-#               print r.content
-#               print "zero if"
+
 
             if (parsed_json['result']) == 1:
                 r = requests.put('http://192.168.0.112:80/WebRelay/api/relays/4', verify=False, json={"state": "on"})
@@ -33,5 +25,3 @@ try:
 
 except URLError, e:
             print "something happened"
-
-
